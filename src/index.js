d786fb172ff8f2fd461827038d75d14775a0d322
Musovvir <mr.muzagov@mail.ru>
2021-06-30 11:27:39 +0300

    add new commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import "./styles.css"
-import {Provider} from "react-redux";
-import {store} from "./redux/configureStore";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/configureStore";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
+      <Provider store={store}>
         <App />
-    </Provider>
+      </Provider>
+    </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
